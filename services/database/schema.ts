80b38ac2dc2b4bdc82ddd618eb31aaa5abf2297a
Carl Osipov <carl.osipov@gmail.com>
2025-02-19 13:26:47 -0500

    schema for event scheduler backend

--- services/database/schema.ts
+++ services/database/schema.ts
@@ -16,8 +16,23 @@ export const messages = sqliteTable("messages", {
   files: text("files", { mode: "json" }).notNull(),
 });
 
+export const events = sqliteTable("events", {
+  id: integer("id").primaryKey({ autoIncrement: true }),
+  chatId: text("chat_id").notNull().references(() => chats.id),
+  createdAt: text("created_at").notNull(),
+  description: text("description").notNull().default(""),
+  prompt: text("prompt").notNull().default(""),
+  frequency: integer("frequency").notNull().default(0),
+  startDelay: integer("start_delay").notNull().default(0),
+  isActive: integer("is_active", { mode: "boolean" }).notNull().default(true),
+  lastRunTime: text("last_run_time"),
+  nextRunTime: text("next_run_time"),
+});
+
 // export types
 export type Chat = typeof chats.$inferSelect;
 export type NewChat = typeof chats.$inferInsert;
 export type Message = typeof messages.$inferSelect;
 export type NewMessage = typeof messages.$inferInsert;
+export type Event = typeof events.$inferSelect;
+export type NewEvent = typeof events.$inferInsert;
